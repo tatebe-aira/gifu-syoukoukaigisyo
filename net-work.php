c0c0e843d3c78f0d36eeb82fa94a2d4f28020a3f
tatebe-aira <tatebe@neko-te.co.jp>
2022-12-01 15:39:59 +0900

    リンク切れ修正

---
+++ net-work.php
@@ -0,0 +1,83 @@
+<link href="https://fonts.googleapis.com/css2?family=Mohave:ital,wght@0,300;0,400;0,500;1,300&family=Ropa+Sans&display=swap" rel="stylesheet">
+<style>
+    .boxwrap {
+        width: 1100px;
+        margin: 0 auto;
+    }
+
+    @media screen and (max-width:768px) {
+        .boxwrap {
+            width: 100%;
+        }
+    }
+</style>
+<div class="netWork-detail-page netWork-page">
+    <div class="mv-img"><img alt="岐阜県事業承継・引継ぎ支援センター ネットワーク事業" class="pc" src="/banner/network2.jpg" /></div>
+    <div class="netWork-info-ttl">
+        <h2>岐阜県事業承継ネットワーク事業</h2>
+    </div>
+    <div class="netWork-about-area boxwrap">
+        <div class="netWork-about-top">
+            <p>岐阜県事業承継ネットワーク事業は、円滑な事業承継を促進するため、県内の市町村、商工会・商工会議所、金融機関、士業団体などのネットワーク化を図るもので、岐阜県では平成29年度より事業承継ネットワーク事業を開始しました。<br>
+                令和３年度より岐阜県事業承継・引継ぎ支援センターが事務局となり、令和４年年度からは岐阜県の支援のもとで運営しています。&nbsp;</p>
+        </div>
+        <div class="netWork-about-bottom netWork-about-flexBox">
+            <div class="netWork-about-left">
+                <p>本事業では</p>
+                <p>（1）事業承継支援に関する各団体との連携<br />
+                    （2）事業承継診断（アンケート）の実施<br />
+                    （3）個別事業者への支援<br />
+                    （4）事業承継ネットワーク会議の開催
+                </p>
+                <br>
+                <p>を行っております。</p>
+            </div>
+            <div class="netWork-about-right">
+                <img src="/_files/00000567/network_meeting.jpg" alt="ネットワーク構成図">
+                <p>令和４年度<br>第2回 岐阜県事業承継ネットワーク会議<br>［2022年11月15日開催］</p>
+            </div>
+        </div>
+    </div>
+    <div class="netWork-info-ttl">
+        <h2>岐阜県事業承継ネットワーク構成図</h2>
+    </div>
+    <div class="boxwrap">
+        <div class="netWork-kousei-img">
+            <p style="text-align:center">
+                <img alt="ネットワーク構成図" src="/_files/00000567/network-kousei.jpg" title="network-kousei" class="pc" />
+                <img alt="ネットワーク構成図" src="/_files/00000567/sp_network-kousei.jpg" title="network-kousei" class="sp" />
+            </p>
+        </div>
+        <div class="netWork-list-link">
+            <a href="/_files/00000567/network-itiran.pdf" target="_blank">
+                <img src="/_files/00000567/network-list.jpg" alt="岐阜県事業承継ネットワーク支援機関一覧はこちら" class="pc">
+                <img src="/_files/00000567/sp_network-list.jpg" alt="岐阜県事業承継ネットワーク支援機関一覧はこちら" class="sp">
+
+            </a>
+        </div>
+        <div class="netWork-list-flex">
+            <a href="https://www.gcci.or.jp/" class="netWork-list-item"><img alt="岐阜商工会議所のサイトへ" src="/_files/00000567/gifushoukoukaigisho.jpg" title="岐阜商工会議所のサイトへ" /></a>
+            <a href="https://www.gifushoko.or.jp/" class="netWork-list-item"><img alt="商工会連合会のサイトへ" src="/_files/00000567/shoukoukairengoukai.jpg" title="商工会連合会のサイトへ" /></a>
+            <a href="https://www.gpc-gifu.or.jp/" class="netWork-list-item"><img alt="産経センターのサイトへ" src="/_files/00000567/sankeicenter.jpg" title="産経センターのサイトへ" /></a>
+        </div>
+    </div>
+    <div class="access-area">
+        <h2 class="access-ttl">
+            アクセス<br><span class="access-ttl-en">ACCESS</span>
+            <img src="/_files/00000567/access_img.gif" alt="">
+        </h2>
+        <div class="access-info">
+            <p>
+                岐阜商工会議所　岐阜県事業承継引継ぎ支援センター<br>
+                <sapn class="access-info-btm">〒500-8727　岐阜市神田町2-2 岐阜商工会議所3F</sapn>
+            </p>
+            <div class="access-tel-flex">
+                <div class="access-tel-img"><img src="/_files/00000567/tell_ico.gif" alt=""></div>
+                <p>058-214-2940</p>
+            </div>
+        </div>
+    </div>
+    <div class="map-area">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3733.9560852243244!2d136.761985!3d35.421708!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6003a9445f0a061d%3A0x29c1eb963ed20ab5!2z5bKQ6Zic5ZWG5bel5Lya6K2w5omA!5e1!3m2!1sja!2sjp!4v1670229200865!5m2!1sja!2sjp" width="100%" height="700px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
---
+++ top.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="ja" prefix="og: http://ogp.me/ns#">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <title>岐阜県事業承継・引継ぎ支援センター</title>
+  <meta name="viewport" content="width=device-width" />
+  <meta name="format-detection" content="telephone=no" />
+  <link rel="stylesheet" href="/_themes/css/normalize.css" />
+  <link rel="stylesheet" href="/_themes/css/common.css" />
+  <link rel="stylesheet" href="/_themes/css/site-color/default.css" />
+  <link rel="alternate stylesheet" href="/_themes/css/colors/white.css" title="white" />
+  <link rel="alternate stylesheet" href="/_themes/css/colors/blue.css" title="blue" />
+  <link rel="alternate stylesheet" href="/_themes/css/colors/black.css" title="black" />
+  <link rel="stylesheet" href="/_themes/css/print.css" media="print" />
+  <script src="/_themes/js/jquery.min.js"></script>
+  <script src="/_themes/js/jquery-migrate.min.js"></script>
+  <script src="/_themes/js/jquery.cookie.min.js"></script>
+  <script src="/_themes/js/navigation.js"></script>
+  <script src="/_themes/js/slick.min.js"></script>
+  <script src="/_themes/js/common.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-FWMG0X71BL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-FWMG0X71BL');
+  </script>
+
+  <link rel="stylesheet" media="screen" href="/_common/js/flatpickr/themes/light.min.css" />
+  <link rel="stylesheet" media="screen" href="/_common/js/flatpickr/themes/ja.css" />
+  <script src="/_common/js/flatpickr/flatpickr.min.js"></script>
+  <script src="/_common/js/flatpickr/l10n/ja.js"></script>
+  <script src="/_common/js/flatpickr/plugins/scrollPlugin.js"></script>
+  <script src="/_common/js/bowser.min.js"></script>
+
+</head>
+
+<body id="page-index" class="rootdir">
+  <div id="container">
+
+    <header id="pageHeader">
+      <div id="pageHeaderContainer">
+        <div id="pageHeaderBody">
+
+          <!-- skip reading -->
+          <div id="accessibilityTool" class="piece">
+            <div class="pieceContainer">
+              <div class="pieceBody">
+
+                <a id="nocssread" href="#main">本文へ</a>
+
+                <div class="tools clearfix">
+                  <ul>
+                    <li><a href="#navigationRuby" id="navigationRuby">ふりがなをつける</a></li>
+                    <li><a href="#navigationTalk" id="navigationTalk">よみあげる</a><span id="navigationTalkPlayer"></span></li>
+                    <li>文字の大きさ
+                      <ul class="fontSize">
+                        <li><a href="#fontLarge" id="fontLarge" title="文字のサイズを大きくする">拡大</a></li>
+                        <li><a href="#fontMiddle" id="fontMiddle" title="標準の文字サイズに戻す">標準</a></li>
+                        <li><a href="#fontSmall" id="fontSmall" title="文字のサイズを小さくする">縮小</a></li>
+                      </ul>
+                    </li>
+                    <li>背景色
+                      <ul class="themeColor">
+                        <li><a href="#themeBlack" id="themeBlack" title="背景色を黒にする" class="black">黒</a></li>
+                        <li><a href="#themeBlue" id="themeBlue" title="背景色を青にする" class="blue">青</a></li>
+                        <li><a href="#themeWhite" id="themeWhite" title="背景色を元に戻す" class="white">標準</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+
+                  <div id="google_translate_element"></div>
+                  <script>
+                    function googleTranslateElementInit() {
+                      new google.translate.TranslateElement({
+                        pageLanguage: 'ja',
+                        includedLanguages: 'en,ko,zh-CN,zh-TW',
+                        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+                      }, 'google_translate_element');
+                    }
+                  </script>
+                  <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /skip reading -->
+          <div id="commonHeader" class="piece">
+            <div class="pieceContainer">
+              <div class="pieceBody">
+                <h1><a href="/"><img src="/_files/00000048/title.png" alt="岐阜県事業承継引継ぎ支援センター" title="岐阜県事業承継引継ぎ支援センター" /></a></h1>
+
+                <div class="menu">
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- end #pageHeaderContainer -->
+      </div>
+      <!-- end #pageHeader -->
+    </header>
+
+    <nav id="pageNav">
+      <div id="pageNavContainer">
+        <div id="pageNavBody">
+
+          <div id="globalNavi" class="piece">
+            <div class="pieceContainer">
+              <div class="pieceBody">
+                <ul>
+                  <li><a href="/">TOP</a></li>
+                  <li><a href="/daisansha/">第三者承継</a></li>
+                  <li><a href="/shinzokunai/">親族内承継</a></li>
+                  <li><a href="/koukeisha/">後継者人材バンク</a></li>
+                  <li><a href="/hoshoukaijo/">経営者保証解除</a></li>
+                  <li><a href="/about/">スタッフ紹介</a></li>
+                  <li><a href="/network/">ネットワーク事業</a></li>
+                  <li><a href="/contact/toiawase/">申込フォーム</a></li>
+                  <li><a href="/download/">書式ダウンロード</a></li>
+                  <li><a href="/on-site-consultation/">出張相談</a></li>
+                  <li><a href="/QandA/">Q&A</a></li>
+                  <li class="has-child"><a href="/jirei/">事例紹介</a>
+                    <ul class="menu__second-level">
+                      <li class="drop-nav"><a href="https://www.gshc.go.jp/jirei/" class="init-bottom">岐阜県事業承継事例集</a></li>
+                      <li class="drop-nav"><a href="https://www.gshc.go.jp/jirei/other-media.html">他メディア　紹介事例</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div id="keyvisual" class="piece">
+            <div class="pieceContainer">
+              <div class="pieceBody">
+                <ul class="bannerImages">
+                  <li class="banner">
+                    <a target="_self" data-token="WuB9kq1sFb1sCXFWbD9p" data-sp-url="" rel="nofollow" href="/">
+                      <img alt="岐阜県事業承継・引継ぎ支援センターのサイトへようこそ" title="岐阜県事業承継・引継ぎ支援センターのサイトへようこそ" loading="lazy" src="/banner/top.jpg" />
+                    </a>
+                  </li>
+                </ul>
+
+
+
+                <script>
+                  $(function() {
+                    if (bowser && bowser.mobile) {
+                      $('#keyvisual li.banner a').each(function() {
+                        var a = $(this);
+                        if (a.data('sp_url')) {
+                          a.attr('href', a.data('sp_url'));
+                        }
+                      });
+                    }
+                  });
+                </script>
+
+
+              </div>
+            </div>
+            <!-- end .piece -->
+          </div>
+
+
+        </div>
+        <!-- end #pageNavContainer -->
+      </div>
+      <!-- end #pageNav -->
+    </nav>
+
+    <div id="main">
+      <div id="mainContainer">
+
+        <div id="wrapper" class="top">
+
+          <div id="content">
+            <div id="contentContainer">
+              <div id="contentBody">
+
+                <div id="topContent">
+                  <div class="containerBlockLeft">
+                    <div id="top1" class="piece">
+                      <div class="pieceContainer">
+                        <div class="pieceHeader">
+                          <h2>センターのご紹介</h2>
+                        </div>
+                        <div class="pieceBody">
+                          <div id="top1" class="piece">
+                            <div class="pieceContainer">
+
+                              <br />
+                              <p style="text-align:left">岐阜県事業承継・引継ぎ支援センターは、事業承継でお悩みの中小企業や個人事業主の皆さんを支援する相談窓口です。国から委託を受けた公的な機関ですので、安心してお気軽にご相談ください。<br />
+                                <!-- <br />
+<a href="https://www.gshc.go.jp/about/index.html"><span style="color:#0068ff"><strong>　＞＞＞スタッフのご紹介</strong></a></p> -->
+                            </div>
+                          </div>
+                          <!-- end .piece -->
+                        </div>
+
+                        <div id="recentDocsTab" class="piece">
+                          <div class="pieceContainer">
+                            <div class="pieceBody">
+                              <nav class="tabs">
+                                <ul>
+                                  <li class="shinchaku current"><a class="showDocTab6" href="#shinchaku">新着情報</a></li>
+                                </ul>
+                              </nav>
+
+                              <div class="wrapper">
+                                <div class="content">
+                                  <ul id="shinchaku" class="shinchaku">
+                                    <li><span class="title_link"><a href="/docs/2022120200014/">郡上市商工会・恵那商工会議所が開催した創業塾で「後継者人材バンク」事業を紹介しました</a></span>（<span class="update_date">2022年12月05日</span>）</li>
+                                    <li><span class="title_link"><a href="/docs/2022113000010/">恵那市恵南商工会「事業承継セミナー&amp;個別相談会」のご案内</a></span>（<span class="update_date">2022年11月30日</span>）</li>
+                                    <li><span class="title_link"><a href="/docs/2022112800017/">可児商工会議所「事業承継対策セミナー」が開催されました</a></span>（<span class="update_date">2022年11月29日</span>）</li>
+                                    <li><span class="title_link"><a href="/docs/2022112400019/">「オープンネーム・後継者募集イベント」が開催されました。</a></span>（<span class="update_date">2022年11月25日</span>）</li>
+                                    <li><span class="title_link"><a href="/docs/2022111500024/">事業承継個別相談会１２，１月の予定を掲載しました。</a></span>（<span class="update_date">2022年11月15日</span>）</li>
+                                    <li><span class="title_link"><a href="/docs/2022111500017/">令和4年度 第2回 岐阜県事業承継ネットワーク会議を開催しました</a></span>（<span class="update_date">2022年11月15日</span>）</li>
+                                  </ul>
+                                </div>
+
+                                <div class="links">
+                                  <div class="shinchaku">
+                                    <ul class="feed">
+                                      <li><a title="RSSフィードを取得" class="feedRss" href="/docs/index.rss">RSS</a></li>
+                                      <li><a title="Atomフィードを取得" class="feedAtom" href="/docs/index.atom">Atom</a></li>
+                                    </ul>
+                                    <div class="more"><a href="/docs/">一覧へ</a></div>
+                                  </div>
+                                </div>
+                                <!-- end .wrapper -->
+                              </div>
+
+                              <script src="/_common/modules/gp_article/js/doc_tabs.js"></script>
+                              <script>
+                                var docTabs6 = new ArticleDocTabs('recentDocsTab');
+                                $(".showDocTab6").on('click', function(e) {
+                                  e.preventDefault();
+                                  var tabId = $(this).attr("href").replace(/#/, "");
+                                  docTabs6.show(tabId);
+                                });
+                              </script>
+                            </div>
+                          </div>
+                          <!-- end .piece -->
+                        </div>
+
+                        <div id="top2" class="piece">
+                          <div class="pieceContainer">
+
+                            <p style="text-align:left">
+                              <img alt="URL変更のお知らせ" src="/_files/00002271/URL_henkou2.jpg" title="URL変更のお知らせ" /><br />
+
+                              令和4年4月1日よりURLおよびメールアドレスを下記の通り変更しました。お手数ですがお気に入り登録等の設定変更をお願いします。<br />
+                              ご迷惑おかけしますがご理解の程よろしくお願いいたします。<br />
+                              （URL）https://www.gshc.go.jp　（MAIL）gifu-hikitugi@gshc.go.jp<br />
+                            </p>
+
+                            <hr />
+                            <p style="text-align:center"><br />
+                              <img alt="こんなお悩みはありませんか？" src="/_files/00001762/konnaonayamiarimasenka.jpg" title="こんなお悩みはありませんか？" />
+                            </p>
+
+                            <h3 style="text-align:center"><span style="color:#cc0000"><strong>「会社や事業を第三者に譲りたいので相手を探して欲しい」<br />
+                                  「後継者が決まっておらずどうしたらよいか」<br />
+                                  「子供に継がせたいがどのようにしたらよいか」<br />
+                                  「会社や事業を譲り受けたい」<br />
+                                  「事業承継にあたり借入金や保証人について相談したい」<br />
+                                  「事業承継の具体的な手順を教えて欲しい」</strong></span></h3>
+
+                            <p style="text-align:center">&nbsp;</p>
+
+                            <p style="text-align:center"><img alt="↓" src="/_files/00001335/sankaku.jpg" title="矢印" /><br />
+                              &nbsp;</p>
+
+                            <h2 style="text-align:center">岐阜県事業承継・引継ぎ支援センターにお任せください！<br />
+                              <br />
+                              <a href="https://www.gshc.go.jp/daisansha/"><img alt="第三者承継" src="/_files/00001724/icon_daisansha.jpg" title="第三者承継" /></a>　<a href="https://www.gshc.go.jp/shinzokunai/"><img alt="親族内承継" src="/_files/00001700/icon_sinzokunai.jpg" title="親族内承継" /></a>　<a href="https://www.gshc.go.jp/koukeisha/"><img alt="後継者人材バンク" src="/_files/00001717/icon_jinnzaibank.jpg" title="後継者人材バンク" /></a>　<a href="https://www.gshc.go.jp/hoshoukaijo/"><img alt="経営者保証解除" src="/_files/00001731/icon_hoshoukaijo.jpg" title="経営者保証解除" /></a>　
+                            </h2>
+
+                            <hr />
+                            <h2 style="text-align:center"><br />
+                              専門の知識をもつ相談員たちが、親切・丁寧にお応えします。<br />
+                              お気軽にお問合せください。</h2>
+
+                            <p style="text-align:center">&nbsp;</p>
+
+                            <p style="text-align:center"><img alt="(TEL)058-2142940" src="/_files/00001786/TEL2.jpg" title="TEL" /><br />
+                              <a href="/_files/00001946/FAX_1c.pdf"><img alt="(FAX)058-214-2941" src="/_files/00001779/FAX2.jpg" title="FAX" /></a><br />
+                              <a href="https://www.gshc.go.jp/contact/toiawase/"><img alt="(Mail)gifu-hikitugi@gshc.jp" src="/_files/00001793/mail2.jpg" title="申込みフォームへ" /></a><br />
+                              <br />
+                              　<img alt="安心・無料・秘密厳守" src="/_files/00001748/maru2.jpg" title="安心・無料・秘密厳守" /><br />
+                              &nbsp;
+                            </p>
+
+                            <hr />
+                            <p style="text-align:center"><br />
+                              <a href="http://www.gcci.or.jp/index.html"><img alt="岐阜商工会議所のサイトへ" src="/_files/00001816/shoukoukaigisho.jpg" title="岐阜商工会議所のサイトへ" /></a><a href="https://www.gifushoko.or.jp/"><img alt="岐阜県商工会連合会のサイトへ" src="/_files/00001823/shoukoukairengoukai.jpg" title="岐阜県商工会連合会のサイトへ" /></a><a href="https://www.gpc-gifu.or.jp/"><img alt="産業経済振興センターのサイトへ" src="/_files/00001830/sankeicenter.jpg" title="産業経済振興センターのサイトへ" /></a><br />
+
+
+                          </div>
+                        </div>
+
+                      </div>
+                    </div>
+
+                  </div>
+                  <!-- end #contentContainer -->
+                </div>
+                <!-- end #content -->
+              </div>
+
+              <!-- end #wrapper -->
+            </div>
+
+            <!-- end #mainContainer -->
+          </div>
+          <!-- end #main -->
+        </div>
+
+        <footer id="pageFooter">
+          <div id="pageFooterContainer">
+            <div id="pageFooterBody">
+
+              <div id="footerNavi" class="piece">
+                <div class="pieceContainer">
+                  <div class="pieceBody">
+                    <ul>
+                      <li><a href="/sitemap.html">サイトマップ</a></li>
+                      <li><a href="/fixed_docs/privacypolicy.html">プライバシーポリシー</a></li>
+                      <li><a href="/contact/toiawase/">お問い合わせ</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div id="commonFooter" class="piece">
+                <div class="pieceContainer">
+                  <div class="pieceBody">
+
+                    <div class="information">
+                      <img src="/_files/00000550/rogo.png" alt="岐阜県事業承継・引継ぎ支援センター" title="岐阜県事業承継・引継ぎ支援センター" />
+                    </div>
+                    <address>
+                      　〒５００-８７２７<br />
+                      　岐阜市神田町2-2 岐阜商工会議所3F<br />
+                      　TEL：０５８-２１４-２９４０<br />
+                      　FAX：０５８-２１４-２９４１<br />
+                      　E-mail：gifu-hikitugi@gshc.go.jp<br />
+                      　<a href="/access/">▶アクセスはこちら</a><br />
+
+                    </address>
+                  </div>
+                </div>
+              </div>
+              <div id="copyright" class="piece">
+                <div class="pieceContainer">
+                  <div class="pieceBody">
+
+                    <small>&copy;2021 岐阜県事業承継・引継ぎ支援センター</small>
+
+                  </div>
+                </div>
+              </div>
+
+            </div>
+            <!-- end #pageFooterContainer -->
+          </div>
+          <!-- end #pageFooter -->
+        </footer>
+
+        <div id="pageTop">
+          <a href="#top">このページの<br />先頭へ戻る</a>
+        </div>
+
+      </div>
+</body>
+
+</html>
